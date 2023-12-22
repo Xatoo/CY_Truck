9d7269dc0aa9197df3cb87ceef56d0aa745cbfea
Xatoo <78871926+Xatoo@users.noreply.github.com>
2023-12-22 09:53:04 +0100

    Update option_t.c

--- progc/option_t.c
+++ progc/option_t.c
@@ -3,7 +3,38 @@
 #include <string.h>
 
 
+typedef struct _etape{
+	int id_etape;
+	char villeA[30];
+	char villeB[30];
+	struct _etape * fg;
+	struct _etape * fd;
 
+}Etape;
+
+
+typedef struct _trajet{
+	int id_trajet;
+	struct _trajet * fg;
+	struct _trajet * fd;
+	struct Etape * etape;
+	
+
+}Trajet;
+
+
+
+Etape * arbreEtape(int id, char villeA,char villeB){
+	Etape * etape = malloc(sizeof(Etape));
+	if (etape ==NULL){
+		exit(1);
+	etape->id_etape = id;
+	etape->villeA = villeA;
+	etape->villeB = villeB;
+	etape->fg = NULL;
+	etape->fd = NULL;
+	return etape;
+} 
 
 int main(){
 
