1b96626a085732a5bcd4e937f98f10605a2dcd2d
AirSoks <131533464+AirSoks@users.noreply.github.com>
2024-01-08 17:59:23 +0100

    Update option_t.c

--- progc/option_t.c
+++ progc/option_t.c
@@ -3,38 +3,88 @@
 #include <string.h>
 
 
-typedef struct _etape{
-	int id_etape;
-	char villeA[30];
-	char villeB[30];
-	struct _etape * fg;
-	struct _etape * fd;
-
-}Etape;
+typedef struct _arbre{
+	struct _arbre *fg;
+	struct _arbre *fd;
+	struct Trajet *pTrajet;
+	int eq;
+	int ville;
+	int nb_trajet;
+	int nb_trajet_depart;
+} Arbre;
 
 
 typedef struct _trajet{
 	int id_trajet;
-	struct _trajet * fg;
-	struct _trajet * fd;
-	struct Etape * etape;
-	
-
-}Trajet;
+	struct _trajet *pNext;
+} Trajet;
 
 
+int max(int a,int b){
+	return (a>b)? a:b;
+}
+int min(int a,int b){
+	return (a<b)? a:b;
+}
 
-Etape * arbreEtape(int id, char villeA,char villeB){
-	Etape * etape = malloc(sizeof(Etape));
-	if (etape ==NULL){
-		exit(1);
-	etape->id_etape = id;
-	etape->villeA = villeA;
-	etape->villeB = villeB;
-	etape->fg = NULL;
-	etape->fd = NULL;
-	return etape;
-} 
+int max2(int a,int b,int c){
+	return max(max(a,b),c);
+}
+
+int min2(int a,int b,int c){
+	return min(min(a,b),c);
+}
+
+Arbre *creerArbre(int elmt) {
+    Arbre *avl = malloc(sizeof(Arbre));
+    avl->ville = elmt; // A voir pour ça 
+    avl->eq = 1;
+    avl->nb_trajet = 0;
+    avl->nb_trajet_depart = 0;
+    avl->pTrajet = NULL;
+    avl->fg = NULL;
+    avl->fd = NULL;
+
+    return avl;
+}
+
+Arbre *rotationGauche(Arbre *avl) {
+    Arbre *pivot = avl->fd;
+    avl->fd = pivot->fg;
+    pivot->fg = avl;
+
+    int eq_avl = avl->eq;
+    int eq_piv = pivot->eq;
+
+    avl->eq = eq_avl - max(eq_piv, 0) - 1;
+    pivot->eq = min2(eq_avl - 2, eq_avl + eq_piv - 2, eq_piv - 1);
+
+    return pivot;
+}
+
+Arbre *rotationDroite(Arbre *avl) {
+    Arbre *pivot = avl->fg;
+    avl->fg = pivot->fd;
+    pivot->fd = avl;
+
+    int eq_avl = avl->eq;
+    int eq_piv = pivot->eq;
+
+    avl->eq = eq_avl - min(eq_piv, 0) + 1;
+    pivot->eq = max2(eq_avl + 2, eq_avl + eq_piv + 2, eq_piv + 1);
+
+    return pivot; 
+}
+
+Arbre *rotationDoubleGauche(Arbre *avl) {
+    avl->fd = rotationDroite(avl->fd);
+    return rotationGauche(avl);
+}
+
+Arbre *rotationDoubleDroite(Arbre *avl) {  
+    avl->fg = rotationGauche(avl->fg);
+    return rotationDroite(avl);
+}
 
 int main(){
 
