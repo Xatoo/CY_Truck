acad568c5ba66362905f21e2236b71551b3d092c
Xatoo <78871926+Xatoo@users.noreply.github.com>
2024-02-01 17:00:55 +0100

    Update option_s.c

--- progc/option_s/option_s.c
+++ progc/option_s/option_s.c
@@ -6,18 +6,23 @@
 
 int main(){
 	Arbre * avl = NULL;
+	//Initialisation des variables qui vont stockées les données en entrée
 	float d;
 	int id_t;
-	int h=0;
 	
+	int h=0;
+	//recuperation des données 
 	while(scanf("%d;%f", &id_t,&d) == 2){
-		Arbre *p = avl;
-		if (recherche(avl,id_t) == NULL){
+		//recheche si un trajet est déjà dans l'arbre 
+		Arbre *p = recherche(avl,id_t);
+		
+		if (p == NULL){
+			//Si le trajet n'est pas dans l'arbre on l'insère dedans
 			avl = insertion(avl,id_t,h,d);
 			
 		}
 		else{
-			p=recherche(avl,id_t);
+			//Si le trajet est dans l'arbre on met à jour les infos du noeud concerné
 			maj(d,p);
 		
 		 }
@@ -26,9 +31,8 @@ int main(){
 	}
 	
 	
-	plusGrandesValeurs(avl);
-	libererArbre(avl);
+	plusGrandesValeurs(avl);  //Recherche des 50 plus grandes valeurs (max - min)
+	libererArbre(avl);  //Liberation de la mémoire utilisé pour l'arbre
 
 	return 0;
 }
-
